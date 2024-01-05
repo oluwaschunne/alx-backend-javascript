726256c4ac1ac553bb5ea64d38921a238f6212c8
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-05 13:35:23 +0000

    js promises

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const ans = mathFunction();
+    queue.push(ans);
+  } catch (e) {
+    queue.push(`${e.name}: ${e.message}`);
+  } finally {
+    queue.push("Guardrail was processed");
+  }
+
+  return queue;
+}

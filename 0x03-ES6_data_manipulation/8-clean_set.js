bbfd11e39f296978783dec7c4608a6aed384bbec
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-13 17:57:06 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,16 @@
 export default function cleanSet(sett, startString) {
-  if (!startString || typeof startString !== "string") return "";
+  if (!startString || typeof startString !== 'string') return '';
   const arr = [];
   let check = false;
   for (const nm of sett) {
     if (
-      typeof nm === "string" &&
-      nm.slice(0, startString.length) === startString
+      typeof nm === 'string'
+      && nm.slice(0, startString.length) === startString
     ) {
       arr.push(nm.slice(startString.length));
       check = true;
     }
   }
-  if (check) return arr.join("-");
-  else return;
+  if (check) return arr.join('-');
+  return '';
 }

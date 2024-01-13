b8847887cc4271d528ee4b2fff680ae37a9f04f2
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-13 18:09:17 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -2,9 +2,9 @@ export default function hasValuesFromArray(sett, arr) {
   let num = 0;
   sett.forEach((x) => {
     for (const n of arr) {
-      if (n === x) num++;
+      if (n === x) num += 1;
     }
   });
   if (num === arr.length) return true;
-  else return false;
+  return false;
 }

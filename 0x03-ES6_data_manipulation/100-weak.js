ab3fa352b4ea6c25e1f807218a8b7d343c86f3d9
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-13 17:33:06 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     const data = weakMap.get(endpoint);
     if (data >= 4) {
-      throw new Error("Endpoint load is high");
+      throw new Error('Endpoint load is high');
     }
 
     weakMap.set(endpoint, data + 1);

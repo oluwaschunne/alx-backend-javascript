fa9eca00c46005bcb2e2d33ea88849c81a01b6e4
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-13 17:27:34 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(item) {
+  if (item instanceof Map) {
+    for (const els of item) {
+      if (els[1] === 1) {
+        item.set(els[0], 100);
+      }
+    }
+    return item;
+  }
+  throw new Error("Cannot process");
+}

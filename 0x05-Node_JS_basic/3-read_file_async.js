8234456c5801791f932f78a76442626a5c4bac61
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-02-28 20:40:31 +0000

    Nodejs

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf-8", (err, data) => {
+      if (err) {
+        throw new Error("Cannot load the database");
+      }
+      const val = data.toString().trim().split("\n");
+      val.shift();
+      const stt = val.map((line) => {
+        return line.trim().split(",");
+      });
+      const dict = {};
+
+      const fields = new Set(stt.map((x) => x[3]));
+      for (const field of fields) {
+        dict[field] = [];
+      }
+      stt.forEach((x) => {
+        dict[x[3]].push(x[0]);
+      });
+      console.log(`Number of students: ${val.length}`);
+      Object.keys(dict).forEach((key) => {
+        console.log(
+          `Number of students in ${key}: ${dict[key].length}. List: ${dict[
+            key
+          ].join(", ")}`
+        );
+      });
+      resolve(data);
+    });
+  });
+}
+module.exports = countStudents;
